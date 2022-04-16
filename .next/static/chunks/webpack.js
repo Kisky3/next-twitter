f0e3ef0971750a6824efcd56bec185e6b751fbc0
kisky3 <kisky3@126.com>
2022-04-16 20:19:20 +0900

    :+1: create structure of project

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ac8bfb664680534e"; }
+/******/ 		__webpack_require__.h = function() { return "16ccd6e6f55c0034"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Head>
+        <title>Twitter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        < Sidebar/>
+        {/* Feed */}
+        {/* Widgets */}
+
+        {/* Modal */}
+      </main>
+
+    </div>
   )
 }
