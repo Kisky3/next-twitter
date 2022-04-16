1dea465f56221dec9fe30b22d7d031062b42bdea
kisky3 <kisky3@126.com>
2022-04-16 19:57:32 +0900

    :gear: add some setting

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8c0ad4c20c0b793f"; }
+/******/ 		__webpack_require__.h = function() { return "ac8bfb664680534e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
