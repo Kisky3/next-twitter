00664374e43e615e47c57fc1e429d6df2152ff5d
kisky3 <kisky3@126.com>
2022-05-01 15:25:20 +0900

    :+1: add something

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2e550c66a1c39ac1"; }
+/******/ 		__webpack_require__.h = function() { return "d5b9d5d56bc60694"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Feed.js
+++ components/Feed.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Feed() {
     return (
-        <div>Feed</div>
+        <div className="text-white flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">Feed</div>
     )
 }
 
